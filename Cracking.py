e8adf563aedca1124d2a3de1f3d539dc292d458c
Ork <horc44@gmail.com>
2021-10-24 12:10:32 +0200

    Added multiple person support

--- Cracking.py
+++ Cracking.py
@@ -2,12 +2,24 @@ import keyboard
 from random import randint
 from time import sleep
 from pynput.keyboard import Key, Listener
+import os
 
 class Cracking:
+
+    number_of_tries = 1
     
     codes = open('codes.txt', 'r').readlines()
-    count = 0
 
+    people = int(input("How many people: "))
+    person_number = int(input("You are person number: "))
+    count = int(input("Enter the starting count: ")) + person_number - 1
+
+    clear = lambda: os.system('cls')
+    clear()
+
+    print("Press b to try a code!")
+
+    
     def crack(self, key):
             
         if hasattr(key, "char"):
@@ -18,15 +30,15 @@ class Cracking:
                     sleep(randint(25,176)/1000)
                     keyboard.press_and_release(number)
 
-                message = "Try#" + str(self.count + 1) + " Code=" + str(self.codes[self.count])
-
+                message = "Try#" + str(self.number_of_tries) + " Code=" + str(self.codes[self.count])
                 print(message)
+
                 f = open("Log.txt", "a")
                 f.write(message)
-                self.count += 1
 
+                self.number_of_tries += 1
+                self.count += self.people
 
-            
     def __init__(self):
         with Listener(on_press = self.crack) as listener:
             listener.join()
